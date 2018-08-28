b840ed8d6c9bd69648539a51f74c7f4177573c50
Ozan Seymen <oseymen@gmail.com>
2018-08-28 23:44:35 +0100

    mocking context

--- myUtil.js
+++ myUtil.js
@@ -1,3 +1,4 @@
 function add(a, b) {
-  return a + b;
+  var c = context.getVariable('c');
+  return (a + b) * c;
 }
--- test.js
+++ test.js
@@ -1,11 +1,29 @@
 var expect = require('expect.js');
+var sinon = require('sinon');
 var rewire = require('rewire');
 
 var app = rewire('./myUtil.js');
 
+var fakeContext = {
+  getVariable: function(s) {}
+}
+
+var contextGetVariableMethod;
+
+beforeEach(function () {
+	contextGetVariableMethod = sinon.stub(fakeContext, 'getVariable');
+});
+
+afterEach(function() {
+	contextGetVariableMethod.restore();
+});
+
 describe('feature: add', function() {
   it('should add two numbers', function() {
+		contextGetVariableMethod.withArgs('c').returns(3);
+    app.__set__('context', fakeContext);
+
     var add = app.__get__('add');
-    expect(add(1,1)).to.equal(2);
+    expect(add(1,1)).to.equal(6);
   });
 });
